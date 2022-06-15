26a7db0757571cedc4fe947fb1f801165ac9d26b
Laura Conde <laurusmayer@gmail.com>
2022-06-15 21:23:04 +0200

    F

--- src/Principal.java
+++ src/Principal.java
@@ -1,6 +1,6 @@
 public class Principal {
     public static void main(String[] args) {
-    int numerito=9; //pues si, al final es un INT
+    int numerito=10; //pues si, al final es un INT
 
         System.out.println(numerito);
 
